45bcdeb48e54fb153bcc17ac67dad211d2a674a1
FlamingoeZpZ <gabekotton@gmail.com>
2025-11-13 21:11:09 -0500

    Update QuickJoinSequence.cs

--- Assets/GabesCommonUtility/Multiplayer/GameObjects/Sequencing/QuickJoinSequence.cs
+++ Assets/GabesCommonUtility/Multiplayer/GameObjects/Sequencing/QuickJoinSequence.cs
@@ -8,8 +8,9 @@ namespace GabesCommonUtility.Multiplayer.GameObjects.Sequencing
 {
     public class QuickJoinSequence : MonoBehaviour, IEntrySequence
     {
-        [SerializeField] private Behaviour success;
-        [SerializeField] private Behaviour noOpenLobbyResponse;
+        [SerializeField] private Behaviour successAsClient;
+        [SerializeField] private bool createNewLobbyOnFail;
+        [SerializeField] private Behaviour successAsHost;
         [SerializeField] private Behaviour failure;
         
         [SerializeField] private int maxLobbySize = 8;
@@ -29,23 +30,42 @@ namespace GabesCommonUtility.Multiplayer.GameObjects.Sequencing
             {
                 if (e.Reason == LobbyExceptionReason.NoOpenLobbies)
                 {
-                    return noOpenLobbyResponse as IEntrySequence;
+                    try
+                    {
+                        //8 Seats total, we restrict seats belonging to our children, excluding our selves. 4 players, but 3 children.
+                        int childCount = 0; //SplitscreenManager.Instance.PlayerCount - 1;
+                        int n = maxLobbySize - childCount;
+
+                        Debug.Log("This lobby will be locked to " + n + " players as there is a child count of: " + (childCount));
+
+                        await LobbySystem.Instance.CreateLobby(n);
+                
+                        Debug.Log("We successfully created the lobby");
+
+                        return successAsHost as  IEntrySequence;
+                    }
+                    catch (LobbyServiceException e2)
+                    {
+                        Debug.LogError("Failed to create lobby: " + e2.Reason);
+                        DisplayMessage?.Invoke("Failed to create lobby: " + e2.Reason);
+                        return failure as  IEntrySequence;
+                    }
                 }
             }
             return failure as IEntrySequence;
         }
 
-        public IEntrySequence Default => success as IEntrySequence;
+        public IEntrySequence Default => successAsClient as IEntrySequence;
         public bool IsCompleted => LobbySystem.Instance.CurrentLobby != null;
 
         private void OnDrawGizmos()
         {
-            if (success && success is not IEntrySequence)
+            if (successAsClient && successAsClient is not IEntrySequence)
             {
                 Debug.LogError("success is INVALID", gameObject);
             }
             
-            if (noOpenLobbyResponse && noOpenLobbyResponse is not IEntrySequence)
+            if (successAsHost && successAsHost is not IEntrySequence)
             {
                 Debug.LogError("noOpenLobbyResponse is INVALID", gameObject);
             }
